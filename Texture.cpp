1c6f79a667805effc548e20f9ba76257944851ee
ひなの 田邊 <220130@jc-21.jp>
2023-06-06 16:45:28 +0900

    テクスチャ

---
+++ Texture.cpp
@@ -0,0 +1,74 @@
+#include "Texture.h"
+#include "DIrect3D.h"
+#include <wincodec.h>
+
+
+#include <DirectXTex.h>
+
+#pragma comment(lib, "DirectXTex.lib")
+
+Texture::Texture() :pSampler_(nullptr), pSRV_(nullptr)
+{
+}
+
+Texture::~Texture()
+{
+	Release();
+}
+
+HRESULT Texture::Load(std::string fileName)
+{
+	using namespace DirectX;
+
+	wchar_t wtext[FILENAME_MAX];
+	size_t ret;
+	mbstowcs_s(&ret, wtext, fileName.c_str(), fileName.length());
+
+	TexMetadata metadata;
+	ScratchImage image;
+	HRESULT hr;
+	hr = LoadFromWICFile(wtext, WIC_FLAGS::WIC_FLAGS_NONE, &metadata, image);
+	if (FAILED(hr))
+	{
+		//
+		MessageBox(nullptr, "画像の読み込みに失敗しました", "エラー", MB_OK);
+		return E_FAIL;
+	}
+
+	//サンプラー作成
+	D3D11_SAMPLER_DESC  SamDesc;
+	ZeroMemory(&SamDesc, sizeof(D3D11_SAMPLER_DESC));
+	SamDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	SamDesc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
+	SamDesc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
+	SamDesc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
+	hr = Direct3D::pDevice->CreateSamplerState(&SamDesc, &pSampler_);
+	if (FAILED(hr))
+	{
+		//エラー処理
+		MessageBox(nullptr, "サンプラーの作成に失敗しました", "エラー", MB_OK);
+		return E_FAIL;
+	}
+
+	//シェーダーリソースビュー
+	D3D11_SHADER_RESOURCE_VIEW_DESC srv = {};
+	srv.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	srv.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+	srv.Texture2D.MipLevels = 1;
+	hr = CreateShaderResourceView(Direct3D::pDevice, image.GetImages(),image.GetImageCount(),metadata, &pSRV_);
+	if (FAILED(hr))
+	{
+		//エラー処理
+		MessageBox(nullptr, "シェーダーリソースビューの作成に失敗しました", "エラー", MB_OK);
+		return E_FAIL;
+	}
+
+	return S_OK;
+	
+}
+
+void Texture::Release()
+{
+	SAFE_RELEASE(pSRV_);
+	SAFE_RELEASE(pSampler_);
+}
