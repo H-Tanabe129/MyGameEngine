e4d1775a995dec77eb86529118ed4e151cbbf4ca
ひなの 田邊 <220130@jc-21.jp>
2023-06-28 13:00:13 +0900

    シェーディング

--- Simple3D.hlsl
+++ Simple3D.hlsl
@@ -53,11 +53,12 @@ VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL)
 //„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ„Ÿ
 float4 PS(VS_OUT inData) : SV_Target
 {
-	float4 lightSource = float4(1.0, 1.0, 1.0, 1.0);
-	float4 ambentSource = float4(0.2, 0.2, 0.2, 1.0);
-	float4 diffuse = g_texture.Sample(g_sampler, inData.uv) * inData.color;
-	float4 ambient = g_texture.Sample(g_sampler, inData.uv) * ambentSource;
-	return g_texture.Sample(g_sampler, inData.uv); //(diffuse + ambient);
-	
+	float4 lightSource = float4(1.0, 1.0, 1.0, 0.0); //RGB
+	float4 ambientSource = float4(0.5, 0.5, 0.5, 1.0);
+	//    return lightSource * g_texture.Sample(g_sampler, inData.uv) * inData.color;
+		//               F«
+		float4 diffuse = lightSource * g_texture.Sample(g_sampler, inData.uv) * inData.color;
+		float4 ambient = lightSource * g_texture.Sample(g_sampler, inData.uv) * ambientSource;
+		return (diffuse + ambient); //g_texture.Sample(g_sampler, inData.uv);
 	//return g_texture.Sample(g_sampler, inData.uv) * inData.color;
 }
